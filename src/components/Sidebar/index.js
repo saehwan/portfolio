010bc9f48eae6450ca9540c94b275eca8e553593
saehwan <tpghks298@naver.com>
2023-09-21 20:54:48 -0500

    uploading

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,14 +1,40 @@
-import {Link} from 'react-router-dom'
+import {Link, NavLink} from 'react-router-dom'
 import './index.scss'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faEnvelope,faHome, faUser} from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 const Sidebar = () => (
     <div className='nav-bar'>
         <Link className = "logo" to="/">
             <img src={LogoS} alt="logo" />
-            <img src={LogoSubtitle} alt= "slobodan" />
+            <img className="sub-logo" src={LogoSubtitle} alt= "slobodan" />
         </Link>
+
+        <nav>
+            <NavLink exact="true" activeclassname ="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e"/>
+            </NavLink>
+            <NavLink exact="true" activeclassname ="active" className="about-link" to="/about">
+                <FontAwesomeIcon icon={faUser} color="#4d4d4e"/>
+            </NavLink>
+            <NavLink exact="true" activeclassname ="active" className="contact-link" to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
+            </NavLink>
+        </nav>
+        <ul>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/saehwan-park-33091822b/'>
+                    <FontAwesomeIcon icon ={faLinkedin} color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://github.com/saehwan'>
+                    <FontAwesomeIcon icon ={faGithub} color="#4d4d4e" />
+                </a>
+            </li>
+        </ul>
     </div>
 )
 export default Sidebar
