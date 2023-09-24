8ee418837c13f76136cb19ed93d3945dd9266c05
saehwan <tpghks298@naver.com>
2023-09-23 23:34:57 -0500

    uploading

---
+++ src/components/About/index.js
@@ -0,0 +1,45 @@
+import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
+import {useState, useEffect} from 'react'
+const About = () => {
+    
+    const [letterClass, setLetterClass] = useState('text-animate')
+
+    useEffect(() => {
+        const idTimeOut = setTimeout(() => {
+          setLetterClass('text-animate-hover')
+        }, 3000)
+    
+        return () => clearTimeout(idTimeOut);
+      }, [])
+
+    return (
+        <div className='container about-page'>
+            <div className='text-zone'>
+                <h1>
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={['A','b','o','u','t',' ','m','e']}
+                        idx={15}
+                    />
+                </h1>
+                <p>
+                    I'm a very ambitious front-end developer looking for a role in an
+                    established IT company with the opportunity to work with the latest
+                    technologies on challenging and diverse projects.
+                </p>
+                <p>
+                    I'm quiet confident, naturally curious, and perpetually working on
+                    improving my chops one design problem at a time.
+                </p>
+                <p>
+                    If I need to define myself in one sentence that would be a family
+                    person, father of a beautiful daughter, a sports fanatic,
+                    photography enthusiast, and tech-obsessed!!!
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default About
