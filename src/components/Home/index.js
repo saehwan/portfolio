bb2289dbcda38858d66424d9097ced873960c50f
saehwan <tpghks298@naver.com>
2023-09-23 20:46:05 -0500

    uploading

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,7 +3,7 @@ import LogoTitle from '../../assets/images/logo-s.png'
 import { Link } from 'react-router-dom'
 import './index.scss';
 import AnimatedLetters from '../AnimatedLetters';
-
+import Logo from './Logo'
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
     const nameArray = "aehwan".split("")
@@ -39,7 +39,7 @@ const Home = () => {
                 <h2>Frontend Developer / JavaScript expert</h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
             </div>
-
+            <Logo/>
         </div>
     )
 }
